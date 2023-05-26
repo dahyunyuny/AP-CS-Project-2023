9dbd0fc07a0df67f63a59302f6b881718b2320d7
EC2 Default User <ec2-user@ip-172-31-47-191.ec2.internal>
2023-05-26 15:56:23 +0000

    thrid push

---
+++ TicTacToe.java
@@ -0,0 +1,163 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class TicTacToe{
+    char[][] board;
+    int x;
+    int y;
+    boolean check;
+    boolean keepGoing;
+    public TicTacToe(){
+    	board = new char[3][3]; //gameboard using 2D array
+        x = 0;
+    	y = 0;
+    	check = true;
+    	keepGoing = true;
+    }
+	public void play(){	
+	    Scanner scan = new Scanner(System.in);
+	    
+	    for(int i =  0;  i< 3; i++){
+            for(int j = 0; j < 3; j++){
+              board[i][j] = ' ';	
+	      	}
+    	}
+	    
+		while(check){
+		    for(int i = 0; i < 3; i++){
+				System.out.println("  "+ board[i][0]+"| "+board[i][1]+" | "+board[i][2]);
+				if(i != 2){
+					System.out.println("---|---|---");
+				}
+			}
+			System.out.println("Player 1's turn: ");
+			System.out.println("Please enter the next coordinates");
+			System.out.println("The X coordinate: ");
+			x = scan.nextInt();
+			System.out.println("The Y coordinate: ");
+			y = scan.nextInt();
+			
+			if(board[x][y] != ' '){
+				System.out.println("Please enter the valid coordinate");
+				continue;
+			}
+			else{
+				board[x][y] = 'X';
+			}
+			if(checkIfDone() == false){
+			    System.out.println("Player 1 Win!!");
+			    break;
+			}
+			for(int i = 0; i < 3; i++){
+				System.out.println("  "+ board[i][0]+"| "+board[i][1]+" | "+board[i][2]);
+				if(i != 2){
+					System.out.println("---|---|---");
+				}
+			}
+			
+			System.out.println("Player 2's turn: ");
+			System.out.println("Please enter the next coordinates");
+			System.out.println("The X coordinate: ");
+			x = scan.nextInt();
+			System.out.println("The Y coordinate: ");
+			y = scan.nextInt();
+			
+			if(board[x][y] != ' '){
+				System.out.println("Please enter the valid coordinate");
+				continue;
+			}
+			else{
+				board[x][y] = 'O';
+			}
+			if(checkIfDone() == false){
+			    System.out.println("Player 2 Win!!");
+			    break;
+			}
+		} 
+    }
+    public boolean checkIfDone(){
+        //case 1
+        if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 2
+        if(board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 3
+        if(board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 4
+        if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 5
+        if(board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 6
+        if(board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 7
+        if(board[0][0]=='X' && board[1][1] == 'X' && board[2][2] =='X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[0][0]=='O' && board[1][1] == 'O' && board[2][2]=='O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        //case 8
+        if(board[0][2]=='X' && board[1][1] == 'X' && board[2][0]=='X'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        else if(board[0][2]=='O' && board[1][1] == 'O' && board[2][0]=='O'){
+            keepGoing = false;
+            return keepGoing;
+        }
+        return true;
+    } 
+    
+
+    public static void main(String[] args){
+        TicTacToe game = new TicTacToe(); // Create an instance of the TicTacToe class
+        game.play();
+    }
+        
+}
+
+
