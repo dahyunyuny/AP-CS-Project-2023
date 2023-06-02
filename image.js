8cb9adea6b3f46d72f3292bbf46e9f8f290ad701
EC2 Default User <ec2-user@ip-172-31-47-191.ec2.internal>
2023-06-02 04:59:05 +0000

    new

--- image.js
+++
@@ -1,7 +0,0 @@
-var floatingImage = document.getElementById('zebra.png');
-
-floatingImage.addEventListener('click', function() {
-  // Perform an action when the floating image is clicked
-  // For example, display an alert
-  alert('Floating image clicked!');
-});
--- tictactoe.js
+++ tictactoe.js
@@ -8,9 +8,12 @@ var board = [
 
 // Variable to keep track of the current player ('X' or 'O')
 var currentPlayer = 'X';
+var playerScore = 0;
+var computerScore = 0;
 
 // Get the audio element
 var clickSound = new Audio('funny.wav');
+var gameWinSound = new Audio('gamewin.wav');
 
 // Add event listeners to the cells
 for (var i = 0; i < cells.length; i++) {
@@ -19,6 +22,10 @@ for (var i = 0; i < cells.length; i++) {
 
 // Function to handle cell click
 function cellClick() {
+  // Check if it's the computer's turn
+  if(currentPlayer === 'O'){
+    return;
+  }
   // Get the row and column index of the clicked cell
   var rowIndex = this.parentNode.rowIndex;
   var cellIndex = this.cellIndex;
@@ -60,6 +67,8 @@ function cellClick() {
         setTimeout(function () {
           drawImage2.parentNode.removeChild(drawImage2);
         }, 3000);
+        
+        playerScore++;
       }
       
       // Reset the game
@@ -69,7 +78,18 @@ function cellClick() {
       currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
       
       if(currentPlayer === 'O'){
-        setTimeout(makeComputerMove,2000);
+        // Remove event listeners temporarily
+        for (var i = 0; i < cells.length; i++) {
+          cells[i].removeEventListener('click', cellClick);
+        }
+
+        setTimeout(function () {
+          makeComputerMove();
+          // Add event listeners back
+          for (var i = 0; i < cells.length; i++) {
+            cells[i].addEventListener('click', cellClick);
+          }
+        }, 2000);
       }
     }
   }
@@ -124,6 +144,7 @@ function makeComputerMove(){
         setTimeout(function () {
           drawImage3.parentNode.removeChild(drawImage3);
         }, 3000);
+        computerScore++;
       }
       // Reset the game
       resetGame();
@@ -183,4 +204,7 @@ function resetGame() {
 
   // Reset the current player
   currentPlayer = 'X';
+  
+  document.getElementById('playerScore').textContent = playerScore;
+  document.getElementById('computerScore').textContent = computerScore;
 }
